f9089ff904e2a6e19f64cbd4cbafc3d44e9edf30
aam051102 <ahlgreenmadsen@gmail.com>
2020-12-19 04:01:55 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# HTML Site Base
+
+My own general template for the development of HTML/CSS/JS sites.
+
+## Usage
+
+```
+npm run dev
+```
+
+Runs local server and watches files.
+
+```
+npm run build
+```
+
+Processes all files and zips them for release.
+
+## Notes
+
+Assets which do not need to be processed go in `/public/`.
---
+++ gulpfile.js
@@ -0,0 +1,126 @@
+const gulp = require("gulp");
+const minify = require("gulp-minify");
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+const connect = require("gulp-connect");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const cleanCSS = require("gulp-clean-css");
+const htmlmin = require("gulp-htmlmin");
+const zip = require("gulp-zip");
+
+sass.compiler = require("node-sass");
+
+function html(next) {
+    gulp.src("./src/html/templates/*.ejs")
+        .pipe(
+            ejs().on("error", (err) => {
+                console.error(err);
+            })
+        )
+        .pipe(
+            htmlmin({
+                /*collapseInlineTagWhitespace: true,*/
+                collapseWhitespace: true,
+                removeTagWhitespace: true,
+                removeComments: true,
+            })
+        )
+        .pipe(
+            rename(function (path) {
+                if (path.basename !== "index") {
+                    path.dirname = path.basename;
+                    path.basename = "index";
+                }
+
+                path.extname = ".html";
+            })
+        )
+        .pipe(gulp.dest("./dist/"))
+        .pipe(connect.reload());
+
+    next();
+}
+
+function public(next) {
+    gulp.src("./public/**/*").pipe(gulp.dest("./dist/")).pipe(connect.reload());
+
+    next();
+}
+
+function scss(next) {
+    gulp.src("./src/css/**/*.scss")
+        .pipe(sass().on("error", (err) => console.error(err)))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("./dist/assets/css"))
+        .pipe(connect.reload());
+
+    next();
+}
+
+function js(next) {
+    gulp.src("./src/js/templates/**/*.js")
+        .pipe(
+            babel({
+                presets: ["@babel/env"],
+            }).on("error", (err) => console.log(err))
+        )
+        .pipe(
+            minify({
+                ext: {
+                    min: ".js",
+                },
+                noSource: true,
+            }).on("error", (err) => console.error(err))
+        )
+        .pipe(gulp.dest("./dist/assets/js"))
+        .pipe(connect.reload());
+
+    next();
+}
+
+function zipRelease(next) {
+    gulp.src("./dist/**/*").pipe(zip("release.zip")).pipe(gulp.dest("builds"));
+
+    next();
+}
+
+// Watchers
+function watchHtml() {
+    gulp.watch("./src/html/**/*.ejs", { ignoreInitial: false }, html);
+}
+
+function watchPublic() {
+    gulp.watch("./public/**/*", { ignoreInitial: false }, public);
+}
+
+function watchScss() {
+    gulp.watch("./src/css/**/*.scss", { ignoreInitial: false }, scss);
+}
+
+function watchJs() {
+    gulp.watch("./src/js/**/*.js", { ignoreInitial: false }, js);
+}
+
+gulp.task("dev", function (next) {
+    watchHtml();
+    watchPublic();
+    watchScss();
+    watchJs();
+    connect.server({
+        livereload: true,
+        root: "dist",
+    });
+
+    next();
+});
+
+gulp.task("build", function (next) {
+    html(next);
+    public(next);
+    scss(next);
+    js(next);
+    zipRelease(next);
+
+    next();
+});
